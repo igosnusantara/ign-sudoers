01983ef8434eda0b1390c2b829889f18dc2bb0bf
wajatmaka <wajatmaka@gmail.com>
2013-08-15 21:08:02 +0700

    Pembenahan direktori

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo 'Pastikan Anda Telah Memasang IGNSDK' 
 cp bin/* /usr/sbin/
 mkdir -p  /usr/share/ign-sdk/test/ign-sudoers.ign/
-cp -rf ../IGN-sudoers/* /usr/share/ign-sdk/test/ign-sudoers.ign/
+cp -rf ../ign-sudoers/* /usr/share/ign-sdk/test/ign-sudoers.ign/
 chmod 644 /usr/share/ign-sdk/test/ign-sudoers.ign/logo.png
 cp  -rf ign-sudoers.desktop /usr/share/applications/
 echo 'Selesai';
